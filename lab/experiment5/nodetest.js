9f81caf78e5fb27b33889f25aecd00cdea71e4e8
ridhisinha03 <ridhisinha345@gmail.com>
2025-09-26 14:08:27 +0530

    folder adddes

---
+++ lab/experiment5/nodetest.js
@@ -0,0 +1,15 @@
+var name="Ridhi"
+
+var email="ridhisinha345@gmail.com"
+
+console.log("Name:"+name)
+console.log("Email:"+email)
+
+var a=10
+var b=20
+
+var sum=a+b
+console.log("sum:"+sum)
+
+var diff=b-a
+console.log("Difference:"+diff)a
